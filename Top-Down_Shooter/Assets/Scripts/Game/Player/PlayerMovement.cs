81839febe589e05a544733bd3f4b7eefc60eba96
ATUDanielW <G00386720@atu.ie>
2025-04-07 15:16:49 +0100

    Movement

---
+++ Top-Down_Shooter/Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMpvement : MonoBehaviour
+{
+
+    [SerializeField] private float _speed;
+    private Rigidbody2D _rigidBody;
+    private Vector2 _movementInput;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidBody.linearVelocity = _movementInput * _speed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+       _movementInput = inputValue.Get<Vector2>();
+    }
+}
