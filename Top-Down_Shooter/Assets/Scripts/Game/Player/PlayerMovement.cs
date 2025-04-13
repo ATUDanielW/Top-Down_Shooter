5a508edd85d1a590873ca920b3c2deec69e4ee75
ATUDanielW <G00386720@atu.ie>
2025-04-13 16:27:50 +0100

    boundry

--- Top-Down_Shooter/Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Top-Down_Shooter/Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -13,10 +13,12 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
+    private Camera _camera;
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _camera = Camera.main;
     }
 
     private void FixedUpdate()
@@ -34,6 +36,26 @@ public class PlayerMovement : MonoBehaviour
                 0.1f);
 
         _rigidBody.linearVelocity = _smoothedMovementInput * _speed;
+
+        PreventPlayerGoingOffScreen();
+        
+        
+    }
+
+    private void PreventPlayerGoingOffScreen()
+    {
+        //passing the position of the player to convert it to screen coordinates
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+        //checking if the player is leaving the border and if so stopping their vlocity left and right X axis
+        if((screenPosition.x <0 && _rigidBody.linearVelocity.x < 0) || (screenPosition.x > _camera.pixelWidth && _rigidBody.linearVelocity.x > 0))
+        {
+            _rigidBody.linearVelocity = new Vector2(0, _rigidBody.linearVelocity.y);
+        }
+        //Top or bottom Y axis
+        if((screenPosition.y <0 && _rigidBody.linearVelocity.y < 0) || (screenPosition.y > _camera.pixelHeight && _rigidBody.linearVelocity.y > 0))
+        {
+            _rigidBody.linearVelocity = new Vector2(_rigidBody.linearVelocity.x, 0);
+        }
     }
     private void RotateInDirectionOfInput()
     {
