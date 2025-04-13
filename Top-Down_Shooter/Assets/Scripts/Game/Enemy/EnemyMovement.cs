fd999dc4130beed68e71b12d590327664bf9c9a1
ATUDanielW <G00386720@atu.ie>
2025-04-13 15:00:35 +0100

    Enemy Wonder

--- Top-Down_Shooter/Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ Top-Down_Shooter/Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Security.Cryptography;
+using NUnit.Framework.Internal;
 using Unity.Mathematics;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -12,11 +16,13 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessController _playerAwarenessController;
     private Vector2 _targetDirection;
+    private float _changeDirectionCooldown;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+        _targetDirection = transform.up;//this means that the firt direction they are moving is the direction they facing
     }
 
     void FixedUpdate()
@@ -28,22 +34,36 @@ public class EnemyMovement : MonoBehaviour
 
     private void UpdateTargetDirection()
     {
+        HandleRandomDirectionChange(); // call this method before the spotting of player so that they wonder around rndomly
+        HandlePlayerTargeting();// call over method that detects player
+        
+    }
+
+    private void HandleRandomDirectionChange()
+    {
+        _changeDirectionCooldown -= Time.deltaTime;
+        if (_changeDirectionCooldown <= 0)
+        {
+            	float angleChange = UnityEngine.Random.Range(-90f, 90f);
+                Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+                _targetDirection = rotation * _targetDirection;
+
+                _changeDirectionCooldown = UnityEngine.Random.Range(1f, 5f);
+        }
+    }
+
+    private void HandlePlayerTargeting()
+    {
         if (_playerAwarenessController.AwareOfPlayer)
         {
             _targetDirection = _playerAwarenessController.DirectionToPlayer;
         }
-        else
-        {
-            _targetDirection = Vector2.zero;
-        }
     }
+
 
     private void RotateTowardsTarget()
     {
-        if (_targetDirection == Vector2.zero)
-        {
-            return;
-        }
+        
 
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
         Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
@@ -54,13 +74,9 @@ public class EnemyMovement : MonoBehaviour
 
     private void SetVelocity()
     {
-        if (_targetDirection == Vector2.zero)
-        {
-            _rigidbody.linearVelocity = Vector2.zero;
-        }else
-        {
+        
             _rigidbody.linearVelocity = transform.up * _speed;
-        }
+        
     }
 }
 
