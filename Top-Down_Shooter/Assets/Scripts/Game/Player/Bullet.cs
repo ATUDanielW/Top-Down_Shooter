181a1d4a6ef5ef9ed41617e5199ca8a4b9623223
ATUDanielW <G00386720@atu.ie>
2025-04-13 20:19:53 +0100

    Destroy bullet

--- Top-Down_Shooter/Assets/Scripts/Game/Player/Bullet.cs
+++ Top-Down_Shooter/Assets/Scripts/Game/Player/Bullet.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+    private Camera _camera;
+    void Update()
+    {
+        DestoryWhenOffScreen();
+    }
+
+    void Awake()
+    {
+        _camera = Camera.main;
+    }
     void OnTriggerEnter2D(Collider2D collision) //whenever collider collides with anything
     {
         //check if bullet collided with enemy, check by cheking enemy movmenet commponent
@@ -11,5 +22,18 @@ public class Bullet : MonoBehaviour
             Destroy (collision.gameObject);
             Destroy(gameObject);
         }
+    }
+
+    private void DestoryWhenOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+        //if these are true bullet will be destoyed
+        if (screenPosition.x <0 ||
+            screenPosition .x > _camera.pixelWidth ||
+            screenPosition.y < 0 ||
+            screenPosition.y > _camera.pixelHeight)
+            {
+                Destroy(gameObject);
+            }
     }
 }
