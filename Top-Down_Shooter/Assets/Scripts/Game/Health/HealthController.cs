335aa3af4fbb9761faeff80ab965c29aa8058187
ATUDanielW <G00386720@atu.ie>
2025-04-14 15:09:15 +0100

    Players Health

---
+++ Top-Down_Shooter/Assets/Scripts/Game/Health/HealthController.cs
@@ -0,0 +1,53 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+   [SerializeField]private float _currentHealth;
+   [SerializeField]private float _maximumHealth;
+
+    public float RemainingHealthPercentage
+    {
+        get
+        {
+            return _currentHealth / _maximumHealth;
+        }
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        if(_currentHealth == 0)
+        {
+            return;
+        }
+
+        _currentHealth -=damageAmount;
+
+        if (_currentHealth < 0)
+        {
+            _currentHealth = 0;
+        }
+
+    }
+
+    public void AddHealth(float amountToAdd)
+    {
+        if (_currentHealth == _maximumHealth)
+        {
+            return;
+        }
+
+        _currentHealth += amountToAdd;
+        if(_currentHealth > _maximumHealth)
+        {
+            _currentHealth = _maximumHealth;
+        }
+    }
+
+
+
+
+
+
+
+}
