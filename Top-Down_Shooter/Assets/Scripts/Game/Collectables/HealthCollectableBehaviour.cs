a4dae723a1737c7539d320764867aac094feb1b6
ATUDanielW <G00386720@atu.ie>
2025-04-21 18:36:39 +0100

    New Assets/Collectables/Score

---
+++ Top-Down_Shooter/Assets/Scripts/Game/Collectables/HealthCollectableBehaviour.cs
@@ -0,0 +1,11 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class HealthCollectableBehaviour : MonoBehaviour, ICollectableBehaviour
+{
+    [SerializeField] private float _healthAmount;
+    public void OnCollected(GameObject player)
+    {
+        player.GetComponent<HealthController>().AddHealth(_healthAmount);
+    }
+}
